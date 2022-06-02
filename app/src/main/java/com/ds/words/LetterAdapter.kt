5dae78f02dd46248878dee7496e2902998390d7a
Vinicius Sampaio <vinicius_travessa@hotmail.com>
2022-06-01 22:33:27 -0300

    !Inicio

---
+++ app/src/main/java/com/ds/words/LetterAdapter.kt
@@ -0,0 +1,11 @@
+package com.ds.words
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LetterAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_letter_adapter)
+    }
+}
