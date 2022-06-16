c5c978258fa8bedfac1d3ef57a381fdc14521fe0
Vinicius Sampaio <vinicius_travessa@hotmail.com>
2022-06-15 22:38:30 -0300

    ok

--- app/src/main/java/com/ds/words/DetailActivity.kt
+++ app/src/main/java/com/ds/words/DetailActivity.kt
@@ -2,10 +2,32 @@ package com.ds.words
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ds.words.databinding.ActivityDetailBinding
+
 
 class DetailActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val letterId = "A"
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = getString(R.string.detail_prefix) + " " + letterId
     }
 }
